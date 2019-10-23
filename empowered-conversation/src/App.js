ed8b54efefd7e153b7dbc25ee0124ee0c3df0675
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2019-10-23 15:14:34 -0400

    add message sent confirmation page

--- empowered-conversation/src/App.js
+++ empowered-conversation/src/App.js
@@ -4,14 +4,16 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Registration from "./components/auth/registration";
 import Login from "./components/auth/login";
 import Home from "./components/Home";
+import MessageConfirm from "./components/MessageConfirm";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/registration" component={Registration} />
+        <Route exact path="/" component={Registration} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/home" component={Home} />
+        <Route exact path="/confirmed" component={MessageConfirm} />
       </Switch>
     </Router>
   );
