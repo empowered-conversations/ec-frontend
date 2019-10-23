5724ec2cbfc87ea814f652cb3e3ffb5e49b68d2e
Aasa-Christian <aasachristian@yahoo.com>
2019-10-23 13:13:18 -0400

    DemoGetData and demoCard

--- empowered-conversation/src/App.js
+++ empowered-conversation/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Registration from "./components/auth/registration";
 import Login from "./components/auth/login";
+import DemoGetData from "./components/demo-component/DemoGetData";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" render={() => <h1>Use Our Tool Page</h1>} />
         <Route exact path="/registration" component={Registration} />
         <Route exact path="/login" component={Login} />
+        <Route  path="/demo" component={DemoGetData}/>
       </Switch>
     </Router>
   );
